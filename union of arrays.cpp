03b1d7e695e237fc541d080538eeed7bae277a70
Samruddhi Bhoyar <52337446+samruddhibhoyar3@users.noreply.github.com>
2021-09-30 07:11:38 +0530

    Create union of arrays.cpp

---
+++ union of arrays.cpp
@@ -0,0 +1,19 @@
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  
+    {
+        map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[a[i]]++;
+        }
+        for(int i=0;i<m;i++)
+        {
+            mp[b[i]]++;
+        }
+        int ans=mp.size();
+        return ans;
+        //code here
+    }
+};
